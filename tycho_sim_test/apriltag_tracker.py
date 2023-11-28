b54e8c67aa9502c9a0770e668406cb7e77a8b329
Quinn Pfeifer <72775766+QuinnP2910@users.noreply.github.com>
2023-11-28 10:46:26 -0800

    Add sim integration

--- tycho_sim_test/apriltag_tracker.py
+++ tycho_sim_test/apriltag_tracker.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 
-from tycho_sim_test.TychoSim import TychoSim
+from TychoSim import TychoSim
 
 
 # Function to detect and track AprilTag
@@ -22,14 +22,15 @@ def track_apriltag(image):
         tag_id = ids[0][0]
 
         cameraMatrix = np.array([[1.38177620e+03, 0.00000000e+00, 5.68619767e+02],
- [0.00000000e+00, 1.44635760e+03, 3.38215946e+02],
- [0.00000000e+00, 0.00000000e+00, 1.00000000e+00]])
+                                 [0.00000000e+00, 1.44635760e+03, 3.38215946e+02],
+                                 [0.00000000e+00, 0.00000000e+00, 1.00000000e+00]])
 
-        distCoeffs = np.array([-2.59102674e-01,  3.74137215e+00, -9.23851828e-04,  7.21012570e-03,
-  -2.99522420e+01])
+        distCoeffs = np.array([-2.59102674e-01, 3.74137215e+00, -9.23851828e-04, 7.21012570e-03,
+                               -2.99522420e+01])
 
         # Estimate pose
-        rvec, tvec, _ = cv2.aruco.estimatePoseSingleMarkers(corners, 1.0, cameraMatrix=cameraMatrix, distCoeffs=distCoeffs)
+        rvec, tvec, _ = cv2.aruco.estimatePoseSingleMarkers(corners, 1.0, cameraMatrix=cameraMatrix,
+                                                            distCoeffs=distCoeffs)
 
         # Convert rotation vector to rotation matrix
         R, _ = cv2.Rodrigues(rvec)
@@ -47,7 +48,7 @@ def track_apriltag(image):
 
 
 # Main function
-def main():
+def main(sim):
     # Open video capture (0 for default camera)
     cap = cv2.VideoCapture(0)
 
@@ -62,6 +63,7 @@ def main():
 
         if result:
             tag_id, x, y, z, yaw, pitch, roll = result
+            simulation_instance.set_leader_position([x, y, z], [yaw, pitch, roll])
             print(f"AprilTag ID: {tag_id}")
             print(f"Position: ({x:.2f}, {y:.2f}, {z:.2f})")
             print(f"Euler Angles: Yaw={yaw:.2f}, Pitch={pitch:.2f}, Roll={roll:.2f}")
@@ -78,9 +80,10 @@ def main():
     cap.release()
     cv2.destroyAllWindows()
 
+
 if __name__ == "__main__":
     simulation_instance = TychoSim(
         [-2.386569349825415, 0.5591684504416656, 0.3236793467739023, -1.78454031627861, 1.5790132769886849,
          -0.8286323184826947, 0.00806077238186087])
     simulation_instance.run_simulation()
-    main()
+    main(simulation_instance)
